1bb893cf24f67889083ef4557639873f1e7da119
qsenn <qsenn1020@gmail.com>
2023-11-14 16:42:48 +0900

    Fix utils.py fixed utils.py imports.

--- utils.py
+++ utils.py
@@ -6,9 +6,9 @@ import re
 import glob
 import numpy as np
 from gym_pcgrl import wrappers
-from stable_baselines import PPO2
-from stable_baselines.bench import Monitor
-from stable_baselines.common.vec_env import SubprocVecEnv, DummyVecEnv
+from stable_baselines3 import PPO
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import SubprocVecEnv, DummyVecEnv
 
 class RenderMonitor(Monitor):
     """
